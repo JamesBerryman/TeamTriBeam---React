522b91385d45cc5dfe1fa9af0e925392d06812f7
JamesBerryman <50531563+JamesBerryman@users.noreply.github.com>
2020-01-08 19:05:16 +0000

    Updates

---
+++ precache-manifest.f90731c9fda59f5e0131e6a7afc69438.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0daebd273d5eedfcc7d8188a2f22ba99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c1a58c83bc8b473caeb",
+    "url": "/static/css/2.0e8573d5.chunk.css"
+  },
+  {
+    "revision": "b82374bdaf4b35c558c5",
+    "url": "/static/css/main.7377cf8f.chunk.css"
+  },
+  {
+    "revision": "6c1a58c83bc8b473caeb",
+    "url": "/static/js/2.cd4664ec.chunk.js"
+  },
+  {
+    "revision": "b82374bdaf4b35c558c5",
+    "url": "/static/js/main.2ec45be8.chunk.js"
+  },
+  {
+    "revision": "0e9a4310ab6a7f7deb27",
+    "url": "/static/js/runtime-main.d91dd2e0.js"
+  },
+  {
+    "revision": "8422da2fdb2e626c2ce50fb6eb0db013",
+    "url": "/static/media/background.8422da2f.jpg"
+  },
+  {
+    "revision": "c3e3e55ac77eabb124e8774da4696a06",
+    "url": "/static/media/dragonball.c3e3e55a.png"
+  },
+  {
+    "revision": "37372a48c19b15613d9763ba75674ee5",
+    "url": "/static/media/facebook.37372a48.png"
+  },
+  {
+    "revision": "556be59b8ae476a6f8da1fb3ebb99b07",
+    "url": "/static/media/heroes2.556be59b.jpg"
+  },
+  {
+    "revision": "6a405f65708ed2245a061d0187dbeb4e",
+    "url": "/static/media/jack.6a405f65.jpg"
+  },
+  {
+    "revision": "64a03d3fe14dd9c3f2a093148aaad29a",
+    "url": "/static/media/james.64a03d3f.jpg"
+  },
+  {
+    "revision": "5ccd60aa4952798d13bc51bdf62fd028",
+    "url": "/static/media/kai.5ccd60aa.jpg"
+  },
+  {
+    "revision": "c38410983921e3b8c02d760c9dc395f8",
+    "url": "/static/media/logo.c3841098.png"
+  },
+  {
+    "revision": "e41bfb7e933698bdf363aab5741ebcd3",
+    "url": "/static/media/twitterimg.e41bfb7e.png"
+  },
+  {
+    "revision": "c88dff8948bdf6b0f0301d042030ff1b",
+    "url": "/static/media/youtube.c88dff89.png"
+  }
+]);
